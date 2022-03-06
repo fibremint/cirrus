61fac27b6c0eea72074fa0b4c2c21ff6e8b4d3bd
Lunar Bridge <horyuurabbit@gmail.com>
2022-03-06 20:55:45 +0900

    Implement prototype audio player (#2)
    * Implement simple audio output
    
    * Draft: audio player
    
    * Draft: Audio player
    
    * Impl: simple audio player
    
    * Impl: audio sample buffer
    
    * Fix: buffer request error
    
    * Update Cargo
    
    * Update Cargo
    
    * Code clean
    
    * Update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use tokio;
+pub mod audio;
+pub mod client;
 
 #[tokio::main]
-async fn run() {
+async fn run() -> Result<(), anyhow::Error> {
+    let mut audio_player = audio::AudioPlayer::new();
+    audio_player.add_audio("test-source2.aiff").await?;
+    println!("main: done add audio");
+    audio_player.play();
+    std::thread::sleep(std::time::Duration::from_secs(5000));
 
+    Ok(())
 }
 
 fn main() {
-    run();
-    // println!("Hello, world!");
+    run().unwrap();
 }
-
