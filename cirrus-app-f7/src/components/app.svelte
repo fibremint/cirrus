631308a49aef1340d97cfa424d8fd3381d005f91
lunarbridge <fibremint@gmail.com>
2022-09-20 17:11:51 +0900

    chore(client): remove unnecessary components

--- cirrus-app-f7/src/components/app.svelte
+++ cirrus-app-f7/src/components/app.svelte
@@ -1,108 +1,21 @@
 <App { ...f7params } >
 
-  <!-- Left panel with cover effect-->
-  <Panel left cover dark>
-    <View>
-      <Page>
-        <Navbar title="Left Panel"/>
-        <Block>Left panel content goes here</Block>
-      </Page>
-    </View>
-  </Panel>
-
-
-  <!-- Right panel with reveal effect-->
-  <Panel right reveal dark>
-    <View>
-      <Page>
-        <Navbar title="Right Panel"/>
-        <Block>Right panel content goes here</Block>
-      </Page>
-    </View>
-  </Panel>
-
-
+ 
   <!-- Your main view, should have "view-main" class -->
   <View main class="safe-areas" url="/" />
 
-
-  <!-- Popup -->
-  <Popup id="my-popup">
-    <View>
-      <Page>
-        <Navbar title="Popup">
-          <NavRight>
-            <Link popupClose>Close</Link>
-          </NavRight>
-        </Navbar>
-        <Block>
-          <p>Popup content goes here.</p>
-        </Block>
-      </Page>
-    </View>
-  </Popup>
-
-  <LoginScreen id="my-login-screen">
-    <View>
-      <Page loginScreen>
-        <LoginScreenTitle>Login</LoginScreenTitle>
-        <List form>
-          <ListInput
-            type="text"
-            name="username"
-            placeholder="Your username"
-            bind:value={username}
-          />
-          <ListInput
-            type="password"
-            name="password"
-            placeholder="Your password"
-            bind:value={password}
-          />
-        </List>
-        <List>
-          <ListButton title="Sign In" onClick={() => alertLoginData()} />
-        </List>
-        <BlockFooter>
-          Some text about login information.<br />Click "Sign In" to close Login Screen
-        </BlockFooter>
-      </Page>
-    </View>
-  </LoginScreen>
 </App>
 <script>
-  import { onDestroy, onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
   import {
-    f7,
     f7ready,
     App,
-    Panel,
-    Views,
     View,
-    Popup,
-    Page,
-    Navbar,
-    Toolbar,
-    NavRight,
-    Link,
-    Block,
-    BlockTitle,
-    LoginScreen,
-    LoginScreenTitle,
-    List,
-    ListItem,
-    ListInput,
-    ListButton,
-    BlockFooter
   } from 'framework7-svelte';
 
-  import { invoke } from '@tauri-apps/api';
-
   import routes from '../js/routes';
-  // import store from '../js/store';
-
-
+ 
   // Framework7 Parameters
   let f7params = {
     name: 'Cirrus', // App name
@@ -117,15 +30,7 @@
       path: '/service-worker.js',
     } : {},
   };
-  // Login screen demo data
-  let username = '';
-  let password = '';
 
-  function alertLoginData() {
-    f7.dialog.alert('Username: ' + username + '<br>Password: ' + password, () => {
-      f7.loginScreen.close();
-    });
-  }
   onMount(() => {
     f7ready(() => {
 
--- cirrus-app-f7/src/pages/home.svelte
+++ cirrus-app-f7/src/pages/home.svelte
@@ -16,55 +16,16 @@
     <Link>Right Link</Link>
   </Toolbar> -->
   <!-- Page content -->
-  <Block strong>
+  <!-- <Block strong>
     <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-  </Block>
+  </Block> -->
 
   <BlockTitle>Navigation</BlockTitle>
   <List>
-    <ListItem link="/about/" title="About"/>
-    <ListItem link="/form/" title="Form"/>
     <ListItem link='/audio-list/' title="Audio list"/>
   </List>
 
-  <BlockTitle>Modals</BlockTitle>
-  <Block strong>
-    <Row>
-      <Col width="50">
-        <Button fill raised popupOpen="#my-popup">Popup</Button>
-      </Col>
-      <Col width="50">
-        <Button fill raised loginScreenOpen="#my-login-screen">Login Screen</Button>
-      </Col>
-    </Row>
-  </Block>
 
-  <BlockTitle>Panels</BlockTitle>
-  <Block strong>
-    <Row>
-      <Col width="50">
-        <Button fill raised panelOpen="left">Left Panel</Button>
-      </Col>
-      <Col width="50">
-        <Button fill raised panelOpen="right">Right Panel</Button>
-      </Col>
-    </Row>
-  </Block>
-
-  <List>
-    <ListItem
-      title="Dynamic (Component) Route"
-      link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-    />
-    <ListItem
-      title="Default Route (404)"
-      link="/load-something-that-doesnt-exist/"
-    />
-    <ListItem
-      title="Request Data & Load"
-      link="/request-and-load/user/123456/"
-    />
-  </List>
 </Page>
 
 <script>
@@ -74,23 +35,10 @@
     NavLeft,
     NavTitle,
     NavTitleLarge,
-    NavRight,
     Link,
-    Toolbar,
-    Block,
     BlockTitle,
     List,
     ListItem,
-    Row,
-    Col,
-    Button
   } from 'framework7-svelte';
 
-  // import { onDestroy } from 'svelte';
-  // import { invoke } from '@tauri-apps/api'
-
-  // onDestroy(() => {
-  //   console.log('destroy');
-  //   invoke('plugin:cirrus|stop_audio');
-  // })
 </script>
