868402044ef80eade4e8f9ebbd159c70603bb0a8
lunarbridge <fibremint@gmail.com>
2022-09-20 18:26:52 +0900

    chore(client): migrate to framework7 + Svelte

--- cirrus-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import App from './App.svelte'
-import 'svelte-material-ui/bare.css';
-
-const app = new App({
-  target: document.getElementById('app')
-})
-
-export default app
--- cirrus-app/src/state.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-import type { AudioTag, AudioPlayerContext } from "./types";
-
-export const audioTagsStore = writable<AudioTag[]>([]);
-export const selectedAudioTagStore = writable<AudioTag>();
-export const audioPlayerContextStore = writable<AudioPlayerContext>();
