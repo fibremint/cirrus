bf442c3ede68052b17cc0fa93f3bd8e6af3f5e55
Dave Kim <fibremint@gmail.com>
2023-03-07 14:23:34 +0900

    check build target os with build-target

--- crates/cirrus-client-core/build.rs
+++ crates/cirrus-client-core/build.rs
@@ -1,3 +1,5 @@
+use build_target;
+
 // ref: https://github.com/rust-windowing/android-ndk-rs/issues/167#issuecomment-1001103028
 // ref: https://github.com/katyo/oboe-rs/issues/28
 fn add_lib(_name: impl AsRef<str>, _static: bool) {
@@ -10,6 +12,9 @@ fn add_lib(_name: impl AsRef<str>, _static: bool) {
 }
 
 fn main() {
-    #[cfg(target_os = "android")]
-    add_lib("c++_shared", false);
+    let target_os = build_target::target_os().unwrap();
+
+    if target_os == build_target::Os::Android {
+        add_lib("c++_shared", false);
+    }
 }
