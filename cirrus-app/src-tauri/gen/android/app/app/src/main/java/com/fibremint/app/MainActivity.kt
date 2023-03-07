f5079229212d9a7daf9c01f362ee67c3bdae4a43
fibremint <fibremint@gmail.com>
2023-03-07 15:14:38 +0900

    generate Android stuffs with Tauri

---
+++ cirrus-app/src-tauri/gen/android/app/app/src/main/java/com/fibremint/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.fibremint.app
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import androidx.annotation.RequiresApi
+
+import app.tauri.plugin.PluginManager
+import app.tauri.plugin.JSObject
+import app.tauri.plugin.Plugin
+
+class MainActivity : TauriActivity() {
+    private var manager: PluginManager? = null
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        manager = PluginManager(this)
+    }
+
+    fun getPluginManager(): PluginManager? {
+        return manager
+    }
+}
+
