521d3a41440653797a37ccdb5c2fa7cd95350380
fibremint <fibremint@gmail.com>
2023-03-07 15:48:24 +0900

    add Tauri kotlin package

---
+++ cirrus-app/src-tauri/gen/android/app/app/src/main/java/com/fibremint/app/generated/RustWebView.kt
@@ -0,0 +1,39 @@
+/* THIS FILE IS AUTO-GENERATED. DO NOT MODIFY!! */
+
+// Copyright 2020-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+package com.fibremint.app
+
+import android.annotation.SuppressLint
+import android.webkit.*
+import android.content.Context
+import android.os.Build
+import kotlin.collections.Map
+
+class RustWebView(context: Context): WebView(context) {
+    init {
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.setGeolocationEnabled(true)
+        settings.databaseEnabled = true
+        settings.mediaPlaybackRequiresUserGesture = false
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        
+    }
+
+    fun loadUrlMainThread(url: String) {
+        post {
+          super.loadUrl(url)
+        }
+    }
+
+    fun loadUrlMainThread(url: String, additionalHttpHeaders: Map<String, String>) {
+        post {
+          super.loadUrl(url, additionalHttpHeaders)
+        }
+    }
+
+    
+}
