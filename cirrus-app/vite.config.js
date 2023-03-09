92df4c0e23e0567e4854192f822b18f8deb5c151
Dave Kim <fibremint@gmail.com>
2023-03-09 17:52:27 +0900

    fix webview failed to reach vite dev server

--- cirrus-app/vite.config.js
+++ cirrus-app/vite.config.js
@@ -1,12 +1,46 @@
 
 import path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-
+import { defineConfig } from 'vite'
+import Unocss from 'unocss/vite'
+import { internalIpV4 } from 'internal-ip'
 
 
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www',);
+const BUILD_DIR = path.resolve(__dirname, './dist',);
+
+// export default defineConfig(async ({ command, mode }) => {
+//   const host = process.env.TAURI_PLATFORM === 'android' || process.env.TAURI_PLATFORM === 'ios' ? (await internalIpV4()) : 'localhost'
+
+//   return {
+//     plugins: [Unocss(), svelte()],
+//     build: {
+//       rollupOptions: {
+//         output: {
+//           entryFileNames: `assets/[name].js`,
+//           chunkFileNames: `assets/[name].js`,
+//           assetFileNames: `assets/[name].[ext]`
+//         }
+//       }
+//     },
+//     server: {
+//       host: '0.0.0.0',
+//       port: 5173,
+//       strictPort: true,
+//       hmr: {
+//         protocol: 'ws',
+//         host,
+//         port: 5183
+//       },
+//       fs: {
+//         allow: ['.', '../../tooling/api/dist']
+//       }
+//     }
+//   }
+// })
+
+const host = process.env.TAURI_PLATFORM === 'android' || process.env.TAURI_PLATFORM === 'ios' ? (await internalIpV4()) : 'localhost'
 
 export default {
   plugins: [
@@ -30,7 +64,16 @@ export default {
     },
   },
   server: {
-    host: true,
-  },
-
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host,
+      port: 5183
+    },
+    fs: {
+      allow: ['.', '../../tooling/api/dist']
+    }
+  }
 };
