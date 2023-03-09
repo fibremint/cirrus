ed3e4a07d83e0343e628bdeae6a75136f2cbeb7a
Dave Kim <fibremint@gmail.com>
2023-03-09 17:52:27 +0900

    fix webview failed to reach vite dev server

--- cirrus-app/workbox-config.js
+++ cirrus-app/workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  globDirectory: 'www/',
+  globDirectory: 'dist/',
   globPatterns: ['**/*.{woff,woff2,js,css,png,jpg,svg,html}'],
   /* pass array of globs to exclude from caching */
   globIgnores: [],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  swDest: 'www/service-worker.js',
+  swDest: 'dist/service-worker.js',
 };
