118bfa76a505e7efb3a9f03a0b72a579890eacb6
Dave Kim <fibremint@gmail.com>
2023-03-25 02:01:37 +0900

    update android project

--- cirrus-app/src-tauri/gen/android/app/app/build.gradle.kts
+++ cirrus-app/src-tauri/gen/android/app/app/build.gradle.kts
@@ -101,8 +101,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
     implementation(project(":tauri-android"))
-
-    implementation(project(":cirrus-tauri-plugin"))
 }
 
 apply(from = "tauri.build.gradle.kts")
