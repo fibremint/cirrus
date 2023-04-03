34e38f38530c1719fdeaac3e16577879faaffcac
Dave Kim <fibremint@gmail.com>
2023-04-03 23:38:48 +0900

    ui: discard service worker

--- cirrus-app/workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  globDirectory: 'dist/',
-  globPatterns: ['**/*.{woff,woff2,js,css,png,jpg,svg,html}'],
-  /* pass array of globs to exclude from caching */
-  globIgnores: [],
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  swDest: 'dist/service-worker.js',
-};
